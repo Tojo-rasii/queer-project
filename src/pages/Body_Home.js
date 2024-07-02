14fe555afb6ec802c4e9a1264c453560abc2eff8
cathyLia03 <liantsoacathy3@gmail.com>
2024-07-02 10:35:56 +0200

    modifier code

--- src/pages/Body_Home.js
+++ src/pages/Body_Home.js
@@ -1,43 +1,40 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 function Body_Home() {
+  const [scrollTops, setScrollTops] = useState(false);
 
-    const [scrollTops, setScrollTops] = useState(false);
-
-
-    const handleTop = () => {
-
-        setScrollTops(!scrollTops);
-        // if(window.scroll(0, 0)){
-        window.scroll(0, 0)
-        // }
-    }
-
-
-
-
-
-
-    return (
-        <div className='Home'>
-            <main>
-                <section>
-                    <article>
-                        <div>
-                            <p>Lorem, ipsum.lorem2 Lorem, ipsum.</p>
-                            <p>Lorem ipsum dolor sit amet Lorem, ipsum.</p>
-                            <button>DISCOVER MORE</button>
-                        </div>
-                    </article>
-                </section>
-            </main>
-            <main className='btn'>
-                <button><i className="bi-arrow-left"></i></button>
-                <button><i className="bi-arrow-right"></i></button>
-            </main>
-            <button id='top' onClick={handleTop}><i className={scrollTops ? 'bi-chevron-up' : 'bi-chevron-down'}></i></button>
-        </div>
-    )
+  const handleTop = () => {
+    setScrollTops(!scrollTops);
+    // if(window.scroll(0, 0)){
+    window.scroll(0, 0);
+    // }
+  };
+  return (
+    <div className="Home">
+      <main>
+        <section>
+          <article>
+            <div>
+              <p>Because love is for everyone.</p>
+              <p>Be proud, be yourself.</p>
+              <button>DISCOVER MORE</button>
+            </div>
+          </article>
+        </section>
+      </main>
+      <main className="btn">
+        <button>
+          <i className="bi-arrow-left"></i>
+        </button>
+        <button>
+          <i className="bi-arrow-right"></i>
+        </button>
+      </main>
+      <button id="top" onClick={handleTop}>
+        <i className={scrollTops ? "bi-chevron-up" : "bi-chevron-down"}></i>
+      </button>
+    </div>
+  );
 }
 
-export default Body_Home
+export default Body_Home;
