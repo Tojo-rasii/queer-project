7e00a18c8b8ae0ed9bd1bf1f11d373361b787ab6
Tojo-rasii <tojorasii1619@gmail.com>
2024-07-02 14:48:06 +0300

    red

--- src/pages/Body_Home.js
+++ src/pages/Body_Home.js
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react'
-
-function Body_Home() {
-
-    const [scrollTops, setScrollTops] = useState(false);
-
-
-    const handleTop = () => {
-
-        setScrollTops(!scrollTops);
-        // if(window.scroll(0, 0)){
-        window.scroll(0, 0)
-        // }
-    }
-
-
-
-
-
-
-    return (
-        <div className='Home'>
-            <main>
-                <section>
-                    <article>
-                        <div>
-                            <p>Lorem, ipsum.lorem2 Lorem, ipsum.</p>
-                            <p>Lorem ipsum dolor sit amet Lorem, ipsum.</p>
-                            <button>DISCOVER MORE</button>
-                        </div>
-                    </article>
-                </section>
-            </main>
-            <main className='btn'>
-                <button><i className="bi-arrow-left"></i></button>
-                <button><i className="bi-arrow-right"></i></button>
-            </main>
-            <button id='top' onClick={handleTop}><i className={scrollTops ? 'bi-chevron-up' : 'bi-chevron-down'}></i></button>
-        </div>
-    )
-}
-
-export default Body_Home
-=======
 import React, { useState, useEffect } from "react";
 
 function Body_Home() {
@@ -83,4 +38,3 @@ function Body_Home() {
 }
 
 export default Body_Home;
->>>>>>> origin/dev
